9cbda77696e51313357be64185158d82bfa6084e
Vftdan <vftdan@gmail.com>
2020-11-13 23:39:48 +0100

    Added --track, --track-total options
    Like eyeD3

--- mutagenc/cli.py
+++ mutagenc/cli.py
@@ -3,7 +3,7 @@
 from mutagen import File
 from mutagen.mp4 import MP4
 from mutagen.mp3 import MP3
-from mutagen.id3 import TPE1, TALB, TIT2, WORS, TDRL, APIC
+from mutagen.id3 import TPE1, TALB, TIT2, WORS, TDRL, APIC, TRCK
 import argparse
 import re
 import os
@@ -12,6 +12,10 @@ import os
 def main():
     parser = argparse.ArgumentParser(
         'Change or manipulate the ID3 tags of the audio files')
+    parser.add_argument('--track', '-n', type=int, default=None,
+                        help='set the track number')
+    parser.add_argument('--track-total', '-N', type=int, default=None,
+                        help='set total number of tracks')
     parser.add_argument('--artist', '-a', default=None,
                         help='set the artist name')
     parser.add_argument('--album', '-A', default=None,
@@ -29,6 +33,7 @@ def main():
                         '''string;
                            the format string should containing one or more'''
                         ''' of the following specifiers:
+                           , {track}
                            , {artist}
                            , {title}
                            , {album}
@@ -50,6 +55,8 @@ def main():
     args = parser.parse_args()
 
     # input section
+    to_track = args.track
+    to_track_total = args.track_total
     to_artist = args.artist
     to_album = args.album
     to_title = args.title
@@ -100,6 +107,24 @@ def main():
                 except:
                     pass
                 # should we set the tag in anyway?
+                if to_track is not None or to_track_total is not None:
+                    old = (0, 0)
+                    if 'TRCK' in file_:
+                        try:
+                            old = tuple(map(int, file_['TRCK'].text[0]
+                                            .split('/')))
+                            old = (old + (0, 0))[:2]
+                        except:
+                            pass
+                    if to_track is None:
+                        to_track = old[0]
+                    if to_track_total is None:
+                        to_track_total = old[1]
+                    file_['TRCK'] = TRCK(encoding=3, text='%02d' % to_track +
+                                         ' / %02d' % to_track_total
+                                         if to_track_total else '')
+                    if to_track == 0 and to_track_total == 0:
+                        del file_['TRCK']
                 if to_artist is not None:
                     file_['TPE1'] = TPE1(encoding=3, text=to_artist)
                 if to_album is not None:
@@ -121,6 +146,8 @@ def main():
                 file_.save()
 
                 # process mp3 specific tag information
+                track = file_['TRCK'].text[0] if 'TRCK' in file_ \
+                    else ''
                 artist = file_['TPE1'].text[0] if 'TPE1' in file_ \
                     else ''
                 album = file_['TALB'].text[0] if 'TALB' in file_ \
@@ -131,6 +158,21 @@ def main():
                 year = file_['TDRL'].text[0] if 'TDRL' in file_ else ''
             elif (type(file_) == MP4):
                 # should we set the tag in anyway?
+                if to_track is not None or to_track_total is not None:
+                    old = (0, 0)
+                    if 'trkn' in file_:
+                        try:
+                            old = tuple(map(int, file_['trkn'][0]))
+                            old = (old + (0, 0))[:2]
+                        except:
+                            pass
+                    if to_track is None:
+                        to_track = old[0]
+                    if to_track_total is None:
+                        to_track_total = old[1]
+                    file_['trkn'] = [(to_track, to_track_total)]
+                    if to_track == 0 and to_track_total == 0:
+                        del file_['trkn']
                 if to_artist is not None:
                     file_['\xa9ART'] = [to_artist]
                 if to_album is not None:
@@ -143,6 +185,8 @@ def main():
                     file_['covr'] = [open(to_cover).read()]
                 file_.save()
 
+                track = '%02d / %02d' % file_['trkn'][0] if 'trkn' in file_ \
+                    else ''
                 artist = file_['\xa9ART'][0] if '\xa9ART' in file_ \
                     else ''
                 album = file_['\xa9alb'][0] if '\xa9alb' in file_ \
@@ -154,7 +198,8 @@ def main():
         except:
             pass
 
-        reps = {'artist': artist,
+        reps = {'track': track,
+                'artist': artist,
                 'title': title,
                 'album': album,
                 'year': year,
